3380d14f6fcb6a2a8d4371fc500c8b77da50a437
Varun Yadav <varunyadav891@gmail.com>
2021-11-24 18:51:20 +0530

    Stacks&Queue

---
+++ Stack & Queue/Queue.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+public:
+	int data;
+	node*next;
+
+	node(int d){
+		data = d;
+		next = NULL;
+	}
+};
+
+class Queue{
+	node*head ,*tail;
+	int len;
+public:
+	Queue(){
+		head = NULL;
+		tail = NULL;
+		len = 0;
+	}
+
+	void push(int d){ //inserton in the end
+		if(head==NULL){
+			head = tail = new node (d);
+		}
+		else{
+			node*n = new node(d);
+			tail->next = n;
+			tail = n;
+		}
+		len++;
+	} 
+
+	void pop(){ //deletion in front
+		if(head==NULL){
+			return;
+		}
+		else if(head->next == NULL){
+			delete head;
+			head = tail = NULL;
+			len--;
+		}
+		else{
+			node*n = head;
+			head= head->next;
+			delete n;
+			len--;
+		}
+	}
+	int front(){
+		return head->data;
+	}
+	bool empty(){
+		return head == NULL;
+	}
+	int size(){
+		return len;
+	}
+};
+
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	Queue q;
+
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	q.push(4);
+	q.push(5);
+
+	while(!q.empty()){
+		cout<<q.front()<<" ";
+		q.pop();
+	}
+	cout<<endl;
+	return 0;
+}
